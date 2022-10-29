63bf31b636d92389854bf03f88ce8b7441a01375
Monty Gole <montygole@gmail.com>
2022-10-29 16:31:16 -0400

    added functionality for infinite outputs now. not done backprop

--- ANN.py
+++ ANN.py
@@ -19,20 +19,18 @@ class NeuralNet:
             for y in self.layers[x].neurons:
                 print(y)
 
-    # def initial_values(self, inputs):
-    #     for layerInd in range(self.layerAmount):
-    #         if self.layers[layerInd].type=="input":
-    #             for n in len(self.layers[layerInd].neurons):
-    #                 self.layers[layerInd].neurons[n].value = inputs[n]
-    #         else:
-    #             for n in len(self.layers[layerInd].neurons):
-    #                 self.layers[layerInd].neurons[n].value = random()
-    
-
     def sigmoid(self, x):
         return 1 / (1 + math.exp(-x)) 
     
+    def derivativeSigmoid(self, x):
+        return (math.exp(-x))/((1+math.exp(-x))**2)
+
+    def meanSquared(self, error):
+        error = 0.5*(error**2)
+        return error
+
     def forwardPass(self, inputs, real):
+        total_error = 0
         for layerInd in range(self.layerAmount):
             neuronNum=0
             print("AT LAYER: ", layerInd, " which is of type: ", self.layers[layerInd].type, "and has a neuron amount of", self.layers[layerInd].neuronAmount)
@@ -54,17 +52,23 @@ class NeuralNet:
                     neuronNum+=1
                     
                 if self.layers[layerInd].type=="output":
-                    output = self.layers[layerInd].neurons[0].value
-        
-        print("The output of this case is:", output)
+                    for outputNeuron in range(len(self.layers[layerInd].neurons)):
+                        output = self.layers[layerInd].neurons[outputNeuron].value
+                        relative_error = output-real[outputNeuron]
+                        self.layers[layerInd].neurons[outputNeuron].error = relative_error
+                        total_error+=self.layers[layerInd].neurons[outputNeuron].error
+
+                    
+        outputs = []
+        for neuron in self.layers[len(self.layers)-1].neurons:
+            outputs.append(neuron.value)
+        print("The output of this case is:", outputs)
         print("The desired output of this case is:", real)
-        print("The error of this case is:", output-real)
+        print("The error of this case is:", total_error)
 
-        return output-real                               
+        return self.meanSquared(total_error)
 
-    def meanSquared(self, error):
-        error = 0.5*(error**2)
-        return error
+
 
     def calculateError(self):
         error = 0
@@ -77,8 +81,12 @@ class NeuralNet:
         print("OVERALL ERROR IS:", error)
         return error
     
-    def backprop(self, error):
-         pass
+    def backprop(self, error, rate):
+        for layer in reversed(self.layers):
+            if layer.type=="output":
+                for neuron in layer.neurons:
+                    for prevN in neuron.receivingFrom:
+                        prevN.weight+=rate*neuron.error*derivativeSigmoid() #CONTINUE HERE
     def __str__(self):
         return f"{self.layerAmount}, {self.layers}"
 class layer:
@@ -89,9 +97,12 @@ class layer:
     def createNeurons(self, next_layer, previous_layer):
         self.neurons = [0]*self.neuronAmount
         for x in range(self.neuronAmount):
-            self.neurons[x]=neuron([random(),random()], 1, next_layer, previous_layer)
-            
-            
+            self.neurons[x]=neuron([], 1, next_layer, previous_layer)
+            if self.type != "output":
+                self.neurons[x].weights = [random()]*next_layer.neuronAmount
+            else:
+                self.neurons[x].error = 0
+
     def __str__(self):
         return f"LAYER:{self.neuronAmount}, {self.type}"
 class neuron:
@@ -109,7 +120,7 @@ input_layer = layer(2, "input")
 h1 = layer(2, "hidden")
 output_layer = layer(1, "output")
 
-net = NeuralNet(3, [input_layer, h1, output_layer], [[[1,0],0], [[0,0],0], [[0,1],0], [[1,1],1]])
+net = NeuralNet(3, [input_layer, h1, output_layer], [[[1,0],[0]], [[0,0],[0]], [[0,1],[0]], [[1,1],[1]]])
 
 net.createLayers()
 net.calculateError()
